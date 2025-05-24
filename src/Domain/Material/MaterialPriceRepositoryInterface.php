cc676abf37f654b2280075c2b59565c54516357d
Ondřej Sochůrek <sochurek@evosoft.cz>
2025-05-24 19:31:55 +0200

    modal component fix

--- src/Domain/Material/MaterialPriceRepositoryInterface.php
+++ src/Domain/Material/MaterialPriceRepositoryInterface.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Domain\Material;
 
+use Doctrine\ORM\QueryBuilder;
+
 interface MaterialPriceRepositoryInterface
 {
+    public function createQueryBuilder(string $alias): QueryBuilder;
+
     public function save(MaterialPrice $price): void;
 
-    public function remove(MaterialPrice $price): void;
+    public function delete(MaterialPrice $price): void;
 }
--- src/Domain/Material/MaterialRepositoryInterface.php
+++ src/Domain/Material/MaterialRepositoryInterface.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Domain\Material;
 
+use Doctrine\ORM\QueryBuilder;
+
 interface MaterialRepositoryInterface
 {
+    public function createQueryBuilder(string $alias): QueryBuilder;
+
     public function findByName(string $name): ?Material;
 
     public function save(Material $material): void;
